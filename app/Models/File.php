085da0cef4016b2ef2fd5c698197bf49c3f8ca05
dingani masikondo <dmasikondo@gmail.com>
2021-08-11 01:21:46 +0200

    fixed display of images and pdf files in comments

--- app/Models/File.php
+++ app/Models/File.php
@@ -36,5 +36,99 @@ class File extends Model
         return $this->belongsTo(Fee::class);
     }
 
+    public function getFileTypeAttribute()
+    {
+        $ext  = pathinfo($this->url, PATHINFO_EXTENSION);
+
+        switch($ext)
+        {
+            case 'mp4':
+            $type='mp4 video';
+            break;
+            case 'mp3':
+            $type='mp3 audio';
+            break;   
+            case 'm4a':
+            $type='m4a audio';
+            break;
+            case 'amr':
+            $type='amr audio';
+            break;
+            case 'aac':
+            $type='aac audio';
+            break;            
+            case 'amr':
+            $type='amr audio';
+            break;                        
+            case 'mov':
+            $type='mov video';
+            break;
+            case 'ogg':
+            $type='ogg video';
+            break;                                    
+            case 'pdf':
+            $type='pdf';
+            break;
+            case 'doc':
+            $type='doc word';
+            break; 
+            case 'docx':
+            $type='docx word';
+            break;   
+            case 'xlsx':
+            $type='xlsx excel';
+            break; 
+            case 'jpg':
+            $type='jpg image';
+            break; 
+            case 'jpeg':
+            $type='jpeg image';
+            break;
+            case 'gif image':
+            $type='gif image';
+            break; 
+            case 'png':
+            $type='png image';
+            break;                                                                               
+            default:
+            $type='image';                                                                                                                                            
+        } 
+
+        return $type;       
+    } 
+
+    public function isImage()
+    {
+        //$ext  = pathinfo($this->url, PATHINFO_EXTENSION);
+        if($this->checkExtension()=='png' || $this->checkExtension()=='bmp' ||$this->checkExtension()=='jpg' || $this->checkExtension()=='jpeg' || $this->checkExtension()=='gif')
+        {
+            return true;
+        }
+    } 
+
+    public function isVideo()
+    {
+        //$ext  = pathinfo($this->url, PATHINFO_EXTENSION);
+        if($this->checkExtension()=='mp4' || $this->checkExtension()=='mov' ||$this->checkExtension()=='ogg')
+        {
+            return true;
+        }
+    } 
+
+    public function isAudio()
+    {
+        //$ext  = pathinfo($this->url, PATHINFO_EXTENSION);
+        if($this->checkExtension()=='mp3' || $this->checkExtension()=='m4a' || $this->checkExtension()=='opus' ||$this->checkExtension()=='amr' 
+            || $this->checkExtension()=='aac')
+        {
+            return true;
+        }        
+    }
+
+    protected function checkExtension()
+    {
+        $ext  = pathinfo($this->url, PATHINFO_EXTENSION);
+        return $ext;
+    }     
 
 }
--- resources/views/livewire/comment/get-comments.blade.php
+++ resources/views/livewire/comment/get-comments.blade.php
@@ -1,6 +1,9 @@
 <div>
     
 @foreach($comments as $comment) 
+{{-- <div class=" flex justify-center items-center">
+  <div class="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-purple-500"></div>
+</div> spin animation--}}
     <div class="grid grid-cols-8 gap-2 my-2">
         <div class="col-span-1">
             <img class="rounded-full w-8" src="{{$comment->user->profile_photo_url}}" />
@@ -18,8 +21,19 @@
             </div>
             <div>
                 {{$comment->body}}
+
             @if($comment->url !='')
-                <img src="/storage/{{$comment->url}}" alt="some files" class="w-16 hover:w-32 cursor-pointer">
+                 @if($comment->isImage()) 
+                    <a href="/storage/{{$comment->url}}" target="_blank">
+                        <img src="/storage/{{$comment->url}}" alt="some files" class="w-16 hover:w-32 cursor-pointer">
+                    </a> 
+                @else
+                    <a href="/storage/{{$comment->url}}" target="_blank">
+                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-24 text-indigo-500  hover:text-white hover:bg-indigo-500 hover:border-4">
+                          <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd"/>
+                        </svg>
+                        <p>Pdf document</p>                        
+                @endif               
             @endif
             </div>
         </div>
