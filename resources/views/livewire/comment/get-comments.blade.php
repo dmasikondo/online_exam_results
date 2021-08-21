d34f7ec43e3670fa48adb79ca0544c399c83e098
dingani masikondo <dmasikondo@gmail.com>
2021-08-21 22:11:13 +0200

    fixed closing tag on getMessage link and changed feesClearance pagination

--- resources/views/livewire/comment/get-comments.blade.php
+++ resources/views/livewire/comment/get-comments.blade.php
@@ -17,7 +17,7 @@
                     {{$comment->user->second_name}} {{$comment->user->first_name}}
                 @endif
                 </span>
-                <span class="text-xs text-gray-400">posted 2 minutes ago</span>
+                <span class="text-xs text-gray-400">posted {{$comment->created_at->diffForHumans()}}</span>
             </div>
             <div>
                 {{$comment->body}}
@@ -32,6 +32,7 @@
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" class="w-24 text-indigo-500  hover:text-white hover:bg-indigo-500 hover:border-4">
                           <path fill-rule="evenodd" d="M4 4a2 2 0 012-2h4.586A2 2 0 0112 2.586L15.414 6A2 2 0 0116 7.414V16a2 2 0 01-2 2H6a2 2 0 01-2-2V4zm2 6a1 1 0 011-1h6a1 1 0 110 2H7a1 1 0 01-1-1zm1 3a1 1 0 100 2h6a1 1 0 100-2H7z" clip-rule="evenodd"/>
                         </svg>
+                    </a>
                         <p>Pdf document</p>                        
                 @endif               
             @endif
